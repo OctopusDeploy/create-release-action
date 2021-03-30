3f3696157f39e455a3b73393ed5122e44cce6932
jbristowe <john.bristowe@octopus.com>
2021-03-30 19:18:10 +1000

    fix: updated tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,16 @@
 import * as inputs from '../src/input-parameters'
+import * as octopus from '../src/create-release'
 
 describe('inputs', () => {
   it('successfully get input parameters', async () => {
     const inputParameters = inputs.get()
     expect(inputParameters != undefined)
+  }, 100000)
+})
+
+describe('releases', () => {
+  it('successfully creates a release', async () => {
+    const inputParameters = inputs.get()
+    octopus.createRelease(inputParameters)
   }, 100000)
 })
--- src/test-setup.ts
+++ src/test-setup.ts
@@ -3,6 +3,9 @@ import * as tmp from 'tmp'
 tmp.setGracefulCleanup()
 const tmpdir = tmp.dirSync({template: 'create-release-XXXXXX'})
 process.env = Object.assign(process.env, {
+  INPUT_API_KEY: process.env['OCTOPUS_APIKEY'],
+  INPUT_SERVER: process.env['OCTOPUS_URL'],
+  INPUT_PROJECT: 'Projects-6445',
   RUNNER_TEMP: tmpdir.name,
   RUNNER_TOOL_CACHE: tmpdir.name,
   GITHUB_ACTION: '1'
