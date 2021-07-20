df9c905a7aaa4a2abc3cb881d14cecac91ee8a5c
jbristowe <john.bristowe@octopus.com>
2021-07-20 12:36:37 +1000

    chore: updated project ID for tests

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -6,7 +6,7 @@ const tmpdir = tmp.dirSync({template: 'create-release-XXXXXX'})
 process.env = Object.assign(process.env, {
   INPUT_API_KEY: process.env['OCTOPUS_APIKEY'],
   INPUT_SERVER: process.env['OCTOPUS_URL'],
-  INPUT_PROJECT: 'Projects-6904',
+  INPUT_PROJECT: 'Projects-7341',
   RUNNER_TEMP: tmpdir.name,
   RUNNER_TOOL_CACHE: tmpdir.name,
   GITHUB_ACTION: '1'
