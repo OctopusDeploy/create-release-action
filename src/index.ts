8a49d4e709898358616b2eac71cd2db4706b129e
jbristowe <john.bristowe@octopus.com>
2021-07-21 18:37:48 +1000

    chore: removed getBooleanInput (redundant)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import * as core from '@actions/core'
-import * as octopus from './create-release'
-import * as inputs from './input-parameters'
+import {setFailed} from '@actions/core'
+import {createRelease} from './create-release'
+import {get} from './input-parameters'
 
 async function run(): Promise<void> {
   try {
-    const inputParameters = inputs.get()
-    await octopus.createRelease(inputParameters)
+    const inputParameters = get()
+    await createRelease(inputParameters)
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message)
   }
 }
 
