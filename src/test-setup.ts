c38c573ec27f92eda0576452f2a4e12c08552f35
jbristowe <john.bristowe@octopus.com>
2021-07-21 18:37:59 +1000

    fix: updated input variables

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -5,6 +5,18 @@ const tmpdir = tmp.dirSync({template: 'create-release-XXXXXX'})
 
 process.env = Object.assign(process.env, {
   INPUT_API_KEY: process.env['OCTOPUS_APIKEY'],
+  INPUT_CANCEL_ON_TIMEOUT: false,
+  INPUT_DEBUG: false,
+  INPUT_DEFAULT_PACKAGE_VERSION: false,
+  INPUT_FORCE: false,
+  INPUT_FORCE_PACKAGE_DOWNLOAD: false,
+  INPUT_IGNORE_CHANNEL_RULES: false,
+  INPUT_IGNORE_EXISTING: false,
+  INPUT_IGNORE_SSL_ERRORS: false,
+  INPUT_NO_RAW_LOG: false,
+  INPUT_PROGRESS: false,
+  INPUT_WAIT_FOR_DEPLOYMENT: false,
+  INPUT_WHAT_IF: false,
   INPUT_SERVER: process.env['OCTOPUS_URL'],
   INPUT_PROJECT: 'Projects-7341',
   RUNNER_TEMP: tmpdir.name,
