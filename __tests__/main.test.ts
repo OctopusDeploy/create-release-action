3172be8ee824441609c02b674b99276c33c39068
jbristowe <john.bristowe@octopus.com>
2021-07-22 06:10:10 +1000

    fix: updated tests to return valid results

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,16 +1,12 @@
 import * as inputs from '../src/input-parameters'
 import * as octopus from '../src/create-release'
 
-describe('inputs', () => {
-  it('successfully get input parameters', async () => {
-    const inputParameters = inputs.get()
-    expect(inputParameters != undefined)
-  }, 100000)
-})
+test('get input parameters', () => {
+  const inputParameters = inputs.get()
+  expect(inputParameters != undefined)
+}, 100000)
 
-describe('releases', () => {
-  it('successfully creates a release', async () => {
-    const inputParameters = inputs.get()
-    octopus.createRelease(inputParameters)
-  }, 100000)
-})
+test('create a release', () => {
+  const inputParameters = inputs.get()
+  return octopus.createRelease(inputParameters)
+}, 100000)
