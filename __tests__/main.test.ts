505b8771e1b92a30399bdb73937d0ff7dbda4463
jbristowe <john.bristowe@octopus.com>
2021-09-18 09:07:43 +1000

    feat: updated tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,8 +3,12 @@ import * as octopus from '../src/create-release'
 
 test('get input parameters', () => {
   const inputParameters = inputs.get()
-  expect(inputParameters != undefined)
-}, 100000)
+  expect(inputParameters).toBeDefined()
+  expect(inputParameters.packages).toBeDefined()
+  expect(inputParameters.packages).toHaveLength(2)
+  expect(inputParameters.packages).toContain('foo:1.2.3-quux')
+  expect(inputParameters.packages).toContain('bar:4.5.6-xyzzy')
+})
 
 test('create a release', () => {
   const inputParameters = inputs.get()
