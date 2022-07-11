7e9a1286dc27772513515887b78bc14a642e92ff
Orion Edwards <orion.edwards@octopus.com>
2022-07-11 13:47:34 +1200

    Remove obsolete arguments from GitHub action

--- src/create-release.ts
+++ src/create-release.ts
@@ -7,128 +7,51 @@ function getArgs(parameters: InputParameters): string[] {
 
   const args = ['create-release']
 
-  if (parameters.apiKey.length > 0) args.push(`--apiKey=${parameters.apiKey}`)
-  if (parameters.cancelOnTimeout) args.push(`--cancelOnTimeout`)
-  if (parameters.channel.length > 0)
+  if (parameters.apiKey.length > 0) {
+    args.push(`--apiKey=${parameters.apiKey}`)
+  }
+  if (parameters.channel.length > 0) {
     args.push(`--channel=${parameters.channel}`)
-  if (parameters.configFile.length > 0)
-    args.push(`--configFile=${parameters.configFile}`)
-  if (parameters.debug) args.push(`--debug`)
-  if (parameters.defaultPackageVersion) args.push(`--defaultPackageVersion`)
-  if (parameters.deployAt.length > 0)
-    args.push(`--deployAt=${parameters.deployAt}`)
-  if (parameters.deployTo.length > 0)
-    args.push(`--deployTo=${parameters.deployTo}`)
-  if (
-    parameters.deploymentCheckSleepCycle.length > 0 &&
-    parameters.deploymentCheckSleepCycle !== `00:00:10`
-  )
-    args.push(
-      `--deploymentCheckSleepCycle=${parameters.deploymentCheckSleepCycle}`
-    )
-  if (
-    parameters.deploymentTimeout.length > 0 &&
-    parameters.deploymentTimeout !== `00:00:10`
-  )
-    args.push(`--deploymentTimeout=${parameters.deploymentTimeout}`)
-  if (parameters.excludeMachines.length > 0)
-    args.push(`--excludeMachines=${parameters.excludeMachines}`)
-  if (parameters.force) args.push(`--force`)
-  if (parameters.forcePackageDownload) args.push(`--forcePackageDownload`)
-  if (
-    parameters.guidedFailure &&
-    parameters.guidedFailure.toLowerCase() === 'true'
-  )
-    args.push(`--guidedFailure=True`)
-  if (parameters.ignoreChannelRules) args.push(`--ignoreChannelRules`)
-  if (parameters.ignoreExisting) args.push(`--ignoreExisting`)
-  if (parameters.ignoreSslErrors) args.push(`--ignoreSslErrors`)
-  if (parameters.logLevel.length > 0 && parameters.logLevel !== `debug`)
-    args.push(`--logLevel=${parameters.logLevel}`)
-  if (parameters.gitRef.length > 0) args.push(`--gitRef=${parameters.gitRef}`)
-  if (parameters.gitCommit.length > 0)
+  }
+  if (parameters.ignoreExisting) {
+    args.push(`--ignoreExisting`)
+  }
+  if (parameters.gitRef.length > 0) {
+    args.push(`--gitRef=${parameters.gitRef}`)
+  }
+  if (parameters.gitCommit.length > 0) {
     args.push(`--gitCommit=${parameters.gitCommit}`)
-  if (parameters.noDeployAfter.length > 0)
-    args.push(`--noDeployAfter=${parameters.noDeployAfter}`)
-  if (parameters.noRawLog) args.push(`--noRawLog`)
-
-  // deprecated
-  if (parameters.package.length > 0) {
-    warning(
-      `"package" input option specified. This option is deprecated and will be removed in a future release. Please use "packages" instead.`
-    )
-    args.push(`--package=${parameters.package}`)
   }
-
-  if (parameters.packages.length > 0)
+  if (parameters.packages.length > 0) {
     parameters.packages.map(p => args.push(`--package=${p}`))
-  if (parameters.packagePrerelease.length > 0)
-    args.push(`--packagePrerelease=${parameters.packagePrerelease}`)
-  if (parameters.packageVersion.length > 0)
+  }
+  if (parameters.packageVersion.length > 0) {
     args.push(`--packageVersion=${parameters.packageVersion}`)
-  if (parameters.packagesFolder.length > 0)
-    args.push(`--packagesFolder=${parameters.packagesFolder}`)
-  if (parameters.password.length > 0) args.push(`--pass=${parameters.password}`)
-  if (parameters.progress) args.push(`--progress`)
-  if (parameters.project.length > 0)
-    args.push(`--project=${parameters.project}`)
-  if (parameters.proxy.length > 0) args.push(`--proxy=${parameters.proxy}`)
-  if (parameters.proxyPassword.length > 0)
+  }
+  if (parameters.proxy.length > 0) {
+    args.push(`--proxy=${parameters.proxy}`)
+  }
+  if (parameters.proxyPassword.length > 0) {
     args.push(`--proxyPass=${parameters.proxyPassword}`)
-  if (parameters.proxyUsername.length > 0)
+  }
+  if (parameters.proxyUsername.length > 0) {
     args.push(`--proxyUser=${parameters.proxyUsername}`)
-  if (parameters.rawLogFile.length > 0)
-    args.push(`--rawLogFile=${parameters.rawLogFile}`)
-  if (parameters.releaseNotes.length > 0)
+  }
+  if (parameters.releaseNotes.length > 0) {
     args.push(`--releaseNotes=${parameters.releaseNotes}`)
-  if (parameters.releaseNotesFile.length > 0)
+  }
+  if (parameters.releaseNotesFile.length > 0) {
     args.push(`--releaseNotesFile=${parameters.releaseNotesFile}`)
-  if (parameters.releaseNumber.length > 0)
+  }
+  if (parameters.releaseNumber.length > 0) {
     args.push(`--releaseNumber=${parameters.releaseNumber}`)
-  if (parameters.server.length > 0) args.push(`--server=${parameters.server}`)
-  if (parameters.skip.length > 0) args.push(`--skip=${parameters.skip}`)
-  if (parameters.space.length > 0) args.push(`--space=${parameters.space}`)
-  if (parameters.specificMachines.length > 0)
-    args.push(`--specificMachines=${parameters.specificMachines}`)
-
-  // deprecated
-  if (parameters.tenant.length > 0) {
-    warning(
-      `"tenant" input option specified. This option is deprecated and will be removed in a future release. Please use "tenants" instead.`
-    )
-    args.push(`--tenant=${parameters.tenant}`)
   }
-
-  if (parameters.tenants.length > 0)
-    parameters.tenants.map(t => args.push(`--tenant=${t}`))
-
-  // deprecated
-  if (parameters.tenantTag.length > 0) {
-    warning(
-      `"tenant_tag" input option specified. This option is deprecated and will be removed in a future release. Please use "tenant_tags" instead.`
-    )
-    args.push(`--tenantTag=${parameters.tenantTag}`)
+  if (parameters.server.length > 0) {
+    args.push(`--server=${parameters.server}`)
   }
-
-  if (parameters.tenantTags.length > 0)
-    parameters.tenantTags.map(t => args.push(`--tenantTag=${t}`))
-  if (parameters.timeout.length > 0 && parameters.timeout !== `600`)
-    args.push(`--timeout=${parameters.timeout}`)
-  if (parameters.username.length > 0) args.push(`--user=${parameters.username}`)
-
-  // deprecated
-  if (parameters.variable.length > 0) {
-    warning(
-      `"variable" input option specified. This option is deprecated and will be removed in a future release. Please use "variables" instead.`
-    )
-    args.push(`--variable=${parameters.variable}`)
+  if (parameters.space.length > 0) {
+    args.push(`--space=${parameters.space}`)
   }
-
-  if (parameters.variables.length > 0)
-    parameters.variables.map(v => args.push(`--variable=${v}`))
-  if (parameters.waitForDeployment) args.push(`--waitForDeployment`)
-  if (parameters.whatIf) args.push(`--whatIf`)
-
   return args
 }
 
--- src/input-parameters.ts
+++ src/input-parameters.ts
@@ -1,111 +1,52 @@
 import { getBooleanInput, getInput, getMultilineInput } from '@actions/core'
 
 export interface InputParameters {
+  // Required
+  project: string
+  // Optional: An API key is required, but you should use the OCTOPUS_API_KEY environment variable instead of this.
   apiKey: string
-  cancelOnTimeout: boolean
   channel: string
-  configFile: string
-  debug: boolean
-  defaultPackageVersion: boolean
-  deployAt: string
-  deployTo: string
-  deploymentCheckSleepCycle: string
-  deploymentTimeout: string
-  excludeMachines: string
-  force: boolean
-  forcePackageDownload: boolean
   gitRef: string
   gitCommit: string
-  guidedFailure: string
-  ignoreChannelRules: boolean
   ignoreExisting: boolean
-  ignoreSslErrors: boolean
-  logLevel: string
-  noDeployAfter: string
-  noRawLog: boolean
-  package: string
-  packagePrerelease: string
   packages: string[]
   packageVersion: string
-  packagesFolder: string
-  password: string
-  progress: boolean
-  project: string
+
+  // Optional: Prefer the OCTOPUS_PROXY environment variable
   proxy: string
+  // Optional: Prefer the OCTOPUS_PROXY_PASSWORD environment variable
   proxyPassword: string
+  // Optional: Prefer the OCTOPUS_PROXY_USERNAME environment variable
   proxyUsername: string
-  rawLogFile: string
+
+  // Optional
   releaseNotes: string
+  // Optional: Overrides release_notes
   releaseNotesFile: string
   releaseNumber: string
+  // Optional: A server is required, but you should use the OCTOPUS_HOST env
   server: string
-  skip: string
+  // Optional: You should prefer the OCTOPUS_SPACE environment variable
   space: string
-  specificMachines: string
-  tenant: string
-  tenants: string[]
-  tenantTag: string
-  tenantTags: string[]
-  timeout: string
-  username: string
-  variable: string
-  variables: string[]
-  waitForDeployment: boolean
-  whatIf: boolean
 }
 
 export function get(): InputParameters {
   return {
     apiKey: getInput('api_key'),
-    cancelOnTimeout: getBooleanInput('cancel_on_timeout'),
     channel: getInput('channel'),
-    configFile: getInput('config_file'),
-    debug: getBooleanInput('debug'),
-    defaultPackageVersion: getBooleanInput('default_package_version'),
-    deployAt: getInput('deploy_at'),
-    deployTo: getInput('deploy_to'),
-    deploymentCheckSleepCycle: getInput('deployment_check_sleep_cycle'),
-    deploymentTimeout: getInput('deployment_timeout'),
-    excludeMachines: getInput('exclude_machines'),
-    force: getBooleanInput('force'),
-    forcePackageDownload: getBooleanInput('force_package_download'),
     gitRef: getInput('git_ref'),
     gitCommit: getInput('git_commit'),
-    guidedFailure: getInput('guided_failure'),
-    ignoreChannelRules: getBooleanInput('ignore_channel_rules'),
     ignoreExisting: getBooleanInput('ignore_existing'),
-    ignoreSslErrors: getBooleanInput('ignore_ssl_errors'),
-    logLevel: getInput('log_level'),
-    noDeployAfter: getInput('no_deploy_after'),
-    noRawLog: getBooleanInput('no_raw_log'),
-    package: getInput('package'),
     packages: getMultilineInput('packages').map(p => p.trim()),
-    packagePrerelease: getInput('package_prerelease'),
     packageVersion: getInput('package_version'),
-    packagesFolder: getInput('packages_folder'),
-    password: getInput('password'),
-    progress: getBooleanInput('progress'),
     project: getInput('project'),
     proxy: getInput('proxy'),
     proxyPassword: getInput('proxy_password'),
     proxyUsername: getInput('proxy_username'),
-    rawLogFile: getInput('raw_log_file'),
     releaseNotes: getInput('release_notes'),
     releaseNotesFile: getInput('release_notes_file'),
     releaseNumber: getInput('release_number'),
     server: getInput('server'),
-    skip: getInput('skip'),
-    space: getInput('space'),
-    specificMachines: getInput('specific_machines'),
-    tenant: getInput('tenant'),
-    tenants: getMultilineInput('tenants').map(p => p.trim()),
-    tenantTag: getInput('tenant_tag'),
-    tenantTags: getMultilineInput('tenant_tags').map(p => p.trim()),
-    timeout: getInput('timeout'),
-    username: getInput('user'),
-    variable: getInput('variable'),
-    variables: getMultilineInput('variables').map(p => p.trim()),
-    waitForDeployment: getBooleanInput('wait_for_deployment'),
-    whatIf: getBooleanInput('what_if')
+    space: getInput('space')
   }
 }
